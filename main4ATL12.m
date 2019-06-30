49a96c9fce9b55ba09f7dfbd2b50959b04a89bd6
Yao Yu <51544390+YaoYu9404@users.noreply.github.com>
2019-06-30 13:22:02 -0700

    Update main4ATL12.m

--- main4ATL12.m
+++ main4ATL12.m
@@ -19,10 +19,10 @@ set(gca, 'FontSize',14)
 
 
 
-%% lat, lon to kml file 
+%% lon, lat to kml file 
 fid = fopen('track1l.xy','w');
 ind = floor(linspace(1,size(track1,1),1000));
-fprintf(fid,'%f %f \n',[track1(ind,1)'; track1(ind,1)']);
+fprintf(fid,'%f %f \n',[track1(ind,1)'; track1(ind,2)']);
 % fprintf(fid,'%f %f \n',[lon(1)'; lat(1)']);
 % fprintf(fid,'>\n');
 fclose(fid);
@@ -31,20 +31,20 @@ fclose(fid);
 
 fid = fopen('track2l.xy','w');
 ind = floor(linspace(1,size(track2,1),1000));
-fprintf(fid,'%f %f \n',[track2(ind,1)'; track2(ind,1)']);
+fprintf(fid,'%f %f \n',[track2(ind,1)'; track2(ind,2)']);
 % fprintf(fid,'%f %f \n',[lon(1)'; lat(1)']);
 % fprintf(fid,'>\n');
 fclose(fid);
-% gmt gmt2kml track2l.xy -Fl  -W3p,red > track2l.kml
+% gmt gmt2kml track2l.xy -Fl  -W2p,red@75 > track2l.kml
 
 
 fid = fopen('track3l.xy','w');
 ind = floor(linspace(1,size(track3,1),1000));
-fprintf(fid,'%f %f \n',[track3(ind,1)'; track3(ind,1)']);
+fprintf(fid,'%f %f \n',[track3(ind,1)'; track3(ind,2)']);
 % fprintf(fid,'%f %f \n',[lon(1)'; lat(1)']);
 % fprintf(fid,'>\n');
 fclose(fid);
-% gmt gmt2kml track3l.xy -Fl  -W3p,blue > track3l.kml
+% gmt gmt2kml track3l.xy -Fl  -W2p,blue@75 > track3l.kml
 
 
 
